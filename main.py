0eaea67fcee0739a0651cb1cc19fe2d22a903f53
Charles <charles@klei.vip>
2024-08-06 18:48:41 +0800

    0.0.1

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import re
 
 from mwclient import Site
-import mwclient
 
 fadom_user_name = ""
 fadom_user_password = ""
@@ -24,17 +23,6 @@ print(f"wikigg登录:{fandom.logged_in}")
 print(f"bwiki登录:{bwiki.logged_in}")
 
 
-def replace_special_chars(input_string):
-    # 定义 Windows 文件命名不允许使用的特殊字符
-    special_chars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*', 'File']
-
-    # 替换特殊字符为 HTML 转义字符
-    for char in special_chars:
-        input_string = input_string.replace(char, "")
-
-    return input_string
-
-
 def update_pages(old_site: Site, new_site: Site):
     one_day_ago = datetime.datetime.now() - datetime.timedelta(days=1)
     end_time = one_day_ago.strftime('%Y-%m-%dT%H:%M:%SZ')
@@ -44,25 +32,25 @@ def update_pages(old_site: Site, new_site: Site):
     page = changes_list_old["query"]["recentchanges"]
     changes_title = []
     for item in page:
-        print(changes_title)
-        if item["title"] in changes_title:
-            continue
-        changes_title.append(item["title"])
-
-        replace_str = r'\[\[(en|ru):[^\]]*\]\]'
-
-        oldpage_text = re.sub(
-            replace_str, "", old_site.pages[item["title"]].text())
-
-        new_site_text = new_site.pages[item["title"]].text()
         try:
+            print(changes_title)
+            if item["title"] in changes_title:
+                continue
+            changes_title.append(item["title"])
+
+            replace_str = r'\[\[(en|ru):[^\]]*\]\]'
+
+            oldpage_text = re.sub(
+                replace_str, "", old_site.pages[item["title"]].text())
+
+            new_site_text = new_site.pages[item["title"]].text()
+
             if oldpage_text != new_site_text:
                 res = new_site.pages[item["title"]].edit(oldpage_text,
                                                          summary=f'原站点{item["title"]}于{item["timestamp"]}由{item["user"]}更改,于此时同步')
                 print(res)
-        except mwclient.errors.APIError as e:
+        except Exception as e:
             print(e)
-            continue
 
 
 update_pages(fandom, bwiki)
