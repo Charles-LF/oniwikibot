97daf28e54c10ae9ee5a682fb92358172266b894
Charles <96990693+Charles-LF@users.noreply.github.com>
2024-08-06 18:30:20 +0800

    Update main.py
    try

--- main.py
+++ main.py
@@ -37,7 +37,8 @@ def replace_special_chars(input_string):
 def update_pages(old_site: Site, new_site: Site):
     one_day_ago = datetime.datetime.now() - datetime.timedelta(days=1)
     end_time = one_day_ago.strftime('%Y-%m-%dT%H:%M:%SZ')
-    changes_list_old = old_site.get(action="query", list="recentchanges", rcend=end_time, rcdir="older", rcprop="user|comment|title|timestamp", rclimit=5)
+    changes_list_old = old_site.get(action="query", list="recentchanges", rcend=end_time,
+                                    rcdir="older", rcprop="user|comment|title|timestamp", rclimit=5)
 
     page = changes_list_old["query"]["recentchanges"]
     changes_title = []
@@ -53,10 +54,14 @@ def update_pages(old_site: Site, new_site: Site):
             replace_str, "", old_site.pages[item["title"]].text())
 
         new_site_text = new_site.pages[item["title"]].text()
-        if oldpage_text != new_site_text:
-            res = new_site.pages[item["title"]].edit(oldpage_text,
-                                                     summary=f'原站点{item["title"]}于{item["timestamp"]}由{item["user"]}更改,于此时同步')
-            print(res)
+        try:
+            if oldpage_text != new_site_text:
+                res = new_site.pages[item["title"]].edit(oldpage_text,
+                                                         summary=f'原站点{item["title"]}于{item["timestamp"]}由{item["user"]}更改,于此时同步')
+                print(res)
+        except Exception as e:
+            print(e)
+            continue
 
 
 update_pages(fandom, bwiki)
