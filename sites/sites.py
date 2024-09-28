fa21aff14e59b6c911a60acd42f8080e4a03c624
Charles <96990693+Charles-LF@users.noreply.github.com>
2024-09-28 22:06:06 +0800

    Update sites.py

--- sites/sites.py
+++ sites/sites.py
@@ -1,3 +1,7 @@
+import sys
+import time
+
+import mwclient
 from mwclient import Site
 
 user_agent = 'CharlesBot/0.0.1 (Charles@klei.vip)'
@@ -7,4 +11,20 @@ wikigg = Site(host='oxygennotincluded.wiki.gg',
 
 bwiki = Site(host='wiki.biligame.com', path="/oni/", clients_useragent=user_agent)
 
+
 # fandom = Site(host="oxygennotincluded.fandom.com", path="/zh/", clients_useragent=user_agent)
+
+def login_to_wiki(site: Site, sessiondata, max_attempts=3):
+    attempts = 0
+    while attempts < max_attempts:
+        try:
+            site.login(cookies={'SESSDATA': sessiondata})
+            if site.logged_in:
+                print("登录成功思密达！！！")
+                return site
+        except mwclient.errors.LoginError as e:
+            print(f"登录次数 {attempts + 1} 失败: {e}")
+            attempts += 1
+            time.sleep(5)  # Wait for 2 seconds before retrying
+    print("多次登录失败，请检查")
+    sys.exit(1)
