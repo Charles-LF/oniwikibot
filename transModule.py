d0df1b8baa81fc2949354e321cf46d09e15d80d6
Charles Lf <charles@klei.vip>
2025-05-20 23:56:42 +0800

    优化模块同步逻辑

--- transModule.py
+++ transModule.py
@@ -1,15 +1,5 @@
-import os
-from pages.transferModule import transModule
-from sites.sites import bwiki, wikigg
-
-wikigg_user_name = ''
-wikigg_user_password = ''
-bwiki_session_data = ''
-
-if 'GITHUB_ACTIONS' in os.environ:
-    wikigg_user_name = os.environ.get('WIKIGG_USER')
-    wikigg_user_password = os.environ.get('WIKIGG_USER_PASSWORD')
-    bwiki_session_data = os.environ.get("BWIKI_SESSION_DATA")
+from pages.transferModule import trans_module
+from sites.sites import wikigg_site, bwiki_site
 
 if __name__ == '__main__':
-    transModule(oldSite=wikigg,newSite=bwiki,username=wikigg_user_name,password=wikigg_user_password,sessiondata=bwiki_session_data)
+    trans_module(old_site=wikigg_site, new_site=bwiki_site)
