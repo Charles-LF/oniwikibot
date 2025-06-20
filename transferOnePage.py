360946d9ea1b3647190b9d8a6911a6ac93620d5e
Charles <96990693+Charles-LF@users.noreply.github.com>
2025-06-20 22:54:47 +0800

    do nothing

--- transferOnePage.py
+++ transferOnePage.py
@@ -1,7 +1,7 @@
 from pages.transferOnePage import transferPage
 from sites.sites import bwiki_site, wikigg_site
 
-pageName = '模块:Data/Buddings'
+pageName = '液桶抽出器'
 
 if __name__ == '__main__':
     transferPage(old_site=wikigg_site, new_site=bwiki_site, page_name=pageName)
