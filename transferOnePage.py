b9ce79d6235ac068fc7ceea1c8fc7eb5ac12350c
Charles Lf <charles@klei.vip>
2025-09-07 22:20:15 +0800

    单个页面同步优化

--- transferOnePage.py
+++ transferOnePage.py
@@ -1,7 +1,8 @@
 from pages.transferOnePage import transferPage
 from sites.sites import bwiki_site, wikigg_site
 
-pageName = '液桶抽出器'
+pageName = ['植物润滑油']
 
 if __name__ == '__main__':
-    transferPage(old_site=wikigg_site, new_site=bwiki_site, page_name=pageName)
+    for name in pageName:
+        transferPage(old_site=wikigg_site, new_site=bwiki_site, page_name=name)
