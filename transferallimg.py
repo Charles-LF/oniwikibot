0cd75382904e54f14a31fa6c70efcb3dbd609902
Charles Lf <charles@klei.vip>
2025-12-21 00:14:08 +0800

    删除缓存共用

--- transferallimg.py
+++
@@ -1,8 +0,0 @@
-from img.img import transferAllImg
-from sites.sites import bwiki_site, wikigg_site
-
-if __name__ == '__main__':
-    print(f"wikigg登录:{wikigg_site.logged_in}")
-    print(f"bwiki登录:{bwiki_site.logged_in}")
-
-    transferAllImg(wikigg_site, bwiki_site)
