a404a5a7c0129d0838297d0c8ce5664a17487f6e
ChiYuKe <114650032+ChiYuKe@users.noreply.github.com>
2026-01-20 20:01:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ if __name__ == '__main__':
     print(f'GG登录情况：{wikigg_site.logged_in}')
     print(f'Bwiki登录情况：{bwiki_site.logged_in}')
     print(f'灰机登录情况：{huiji_site.logged_in}')
-    update_pages(old_site=wikigg_site, new_site=bwiki_site)
+    # update_pages(old_site=wikigg_site, new_site=bwiki_site)
+
+    update_pages(wikigg_site, bwiki_site, max_workers=3)
     # update_pages(old_site=wikigg_site, new_site=huiji_site)
